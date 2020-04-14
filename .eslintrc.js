d2946e1d6dfea5d572c74a7e51b5b55e08e1c026
Roman <rgrabelnikov@yandex.ru>
2020-04-14 14:30:09 +0700

    Prepared project

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,14 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': [
+      'warn',
+      {
+        'vars': 'all',
+        'args': 'after-used',
+        'ignoreRestSiblings': false
+      }
+    ]
   }
 }
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? './' // prod
+    : '/', // dev
+  devServer: {
+    proxy: 'https://pylearn.info'
+    // https: true
+  }
+}
