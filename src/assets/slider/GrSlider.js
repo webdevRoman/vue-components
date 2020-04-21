1ac62ca35f55fc5a9e9919900cd8e5cc5b3456c2
Roman <rgrabelnikov@yandex.ru>
2020-04-21 16:12:37 +0700

    Finished slider

--- src/assets/slider/GrSlider.js
+++ src/assets/slider/GrSlider.js
@@ -1,68 +1,89 @@
 export class GrSlider {
   constructor(options) {
-    this.arrowsEnabled = options.arrowsEnabled
     this.navigationEnabled = options.navigationEnabled
-
     this.wrapper = document.getElementById('gr-slider-wrapper')
     this.container = document.getElementById('gr-slider-container')
     this.items = document.querySelectorAll('.gr-slider-item')
     this.itemsNumber = this.items.length
-
     const cloneFirst = this.items[0].cloneNode(true)
     const cloneLast = this.items[this.itemsNumber - 1].cloneNode(true)
     this.container.appendChild(cloneFirst)
     this.container.insertBefore(cloneLast, this.items[0])
-
-    this.currentItem = 0
     this.itemWidth = this.wrapper.offsetWidth
-    
+    this.currentItem = 0
+    this.allowSlide = true
+
+    this.posX1 = 0
+    this.posX2 = 0
+    this.threshold = 100
+    this.bindedDragEnd = this.dragEnd.bind(this)
+    this.bindedDragAction = this.dragAction.bind(this)
+
     this.container.style.left = -this.itemWidth + 'px'
     this.container.style.transition = '0.2s'
-    this.allowSlide = true
-    
-    if (this.arrowsEnabled) {
-      const arrowLeft = document.createElement('div')
+
+    this.container.addEventListener('mousedown', this.dragStart.bind(this))
+    this.container.addEventListener('touchstart', this.dragStart.bind(this))
+    this.container.addEventListener('touchend', this.dragEnd.bind(this))
+    this.container.addEventListener('touchmove', this.dragAction.bind(this))
+
+    if (options.arrowsEnabled) {
+      const arrowLeft = document.createElement('button')
       arrowLeft.classList.add('gr-slider-arrow')
       arrowLeft.classList.add('gr-slider-arrow_left')
-      const arrowRight = document.createElement('div')
+      const arrowRight = document.createElement('button')
       arrowRight.classList.add('gr-slider-arrow')
       arrowRight.classList.add('gr-slider-arrow_right')
       this.wrapper.appendChild(arrowLeft)
       this.wrapper.appendChild(arrowRight)
+      arrowLeft.addEventListener('click', () => this.slide(this.currentItem - 1))
+      arrowRight.addEventListener('click', () => this.slide(this.currentItem + 1))
+    }
 
-      arrowLeft.addEventListener('click', () => this.slideLeft())
-      arrowRight.addEventListener('click', () => this.slideRight())
+    if (this.navigationEnabled) {
+      this.wrapper.style.height = this.wrapper.offsetHeight + 50 + 'px'
+      this.container.style.height = this.wrapper.offsetHeight - 50 + 'px'
+      this.navItems = []
+      const navContainer = document.createElement('div')
+      navContainer.classList.add('gr-slider-nav')
+      for (let i = 0; i < this.itemsNumber; i++) {
+        const navItem = document.createElement('button')
+        navItem.classList.add('gr-slider-nav__item')
+        this.navItems.push(navItem)
+        navContainer.appendChild(this.navItems[i])
+      }
+      this.navItems[0].classList.add('gr-slider-nav__item_active')
+      this.wrapper.appendChild(navContainer)
+      for (let i = 0; i < this.itemsNumber; i++)
+        this.navItems[i].addEventListener('click', () => this.slide(i))
     }
   }
 
-  slideLeft() {
+  slide(itemNumber) {
     if (this.allowSlide) {
       this.allowSlide = false
       setTimeout(() => { this.allowSlide = true }, 300)
-      this.currentItem--
+      this.currentItem = itemNumber
       this.container.style.left = -(this.currentItem + 1) * this.itemWidth + 'px'
-      setTimeout(() => { this.checkLeftIndex() }, 200)
+      setTimeout(() => { this.checkIndex() }, 200)
+      if (this.navigationEnabled) {
+        this.navItems.forEach(item => { item.classList.remove('gr-slider-nav__item_active') })
+        if (this.currentItem === -1)
+          this.navItems[this.navItems.length - 1].classList.add('gr-slider-nav__item_active')
+        else if (this.currentItem === this.navItems.length)
+          this.navItems[0].classList.add('gr-slider-nav__item_active')
+        else
+          this.navItems[this.currentItem].classList.add('gr-slider-nav__item_active')
+      }
     }
   }
-  slideRight() {
-    if (this.allowSlide) {
-      this.allowSlide = false
-      setTimeout(() => { this.allowSlide = true }, 300)
-      this.currentItem++
-      this.container.style.left = -(this.currentItem + 1) * this.itemWidth + 'px'
-      setTimeout(() => { this.checkRightIndex() }, 200)
-      
-    }
-  }
-  checkLeftIndex() {
+  checkIndex() {
     if (this.currentItem === -1) {
       this.container.style.transition = 'none'
       this.currentItem = this.itemsNumber - 1
       this.container.style.left = -(this.currentItem + 1) * this.itemWidth + 'px'
       setTimeout(() => { this.container.style.transition = '0.2s' }, 80)
     }
-  }
-  checkRightIndex() {
     if (this.currentItem === this.itemsNumber) {
       this.container.style.transition = 'none'
       this.currentItem = 0
@@ -70,115 +91,41 @@ export class GrSlider {
       setTimeout(() => { this.container.style.transition = '0.2s' }, 80)
     }
   }
+
+  dragStart(e) {
+    this.container.style.transition = 'none'
+    e = e || window.event
+    e.preventDefault()
+    this.posInitial = parseInt(this.container.style.left)
+    if (e.type == 'touchstart') {
+      this.posX1 = e.touches[0].clientX
+    } else {
+      this.posX1 = e.clientX
+      document.addEventListener('mouseup', this.bindedDragEnd)
+      document.addEventListener('mousemove', this.bindedDragAction)
+    }
+  }
+  dragAction(e) {
+    e = e || window.event
+    if (e.type === 'touchmove') {
+      this.posX2 = this.posX1 - e.touches[0].clientX
+      this.posX1 = e.touches[0].clientX
+    } else {
+      this.posX2 = this.posX1 - e.clientX
+      this.posX1 = e.clientX
+    }
+    this.container.style.left = (parseInt(this.container.style.left) - this.posX2) + 'px'
+  }
+  dragEnd() {
+    this.container.style.transition = '0.2s'
+    this.posFinal = parseInt(this.container.style.left)
+    if (this.posFinal - this.posInitial < -this.threshold)
+      this.slide(this.currentItem + 1)
+    else if (this.posFinal - this.posInitial > this.threshold)
+      this.slide(this.currentItem - 1)
+    else
+      this.container.style.left = (this.posInitial) + 'px'
+    document.removeEventListener('mouseup', this.bindedDragEnd)
+    document.removeEventListener('mousemove', this.bindedDragAction)
+  }
 }
-
-
-// const container = document.getElementById('gr-slider-container'),
-//   items = document.querySelectorAll('.gr-slider-item'),
-//   prev = document.getElementById('prev'),
-//   next = document.getElementById('next')
-
-// // function slide(container, items, prev, next) {
-// function slide(container, items) {
-//   let posX1 = 0,
-//     posX2 = 0,
-//     posInitial,
-//     posFinal,
-//     threshold = 100,
-//     itemsNumber = items.length,
-//     itemWidth = items[0].offsetWidth,
-//     firstItem = items[0],
-//     lastItem = items[itemsNumber - 1],
-//     firstClone = firstItem.cloneNode(true),
-//     lastClone = lastItem.cloneNode(true),
-//     index = 0,
-//     allowShift = true
-
-//   // Clone first and last slide
-//   container.appendChild(firstClone)
-//   container.insertBefore(lastClone, firstItem)
-
-//   // Mouse events
-//   container.addEventListener('mousedown', dragStart)
-
-//   // Touch events
-//   container.addEventListener('touchstart', dragStart)
-//   container.addEventListener('touchend', dragEnd)
-//   container.addEventListener('touchmove', dragAction)
-
-//   // Click events
-//   // prev.addEventListener('click', () => shiftSlide(-1))
-//   // next.addEventListener('click', () => shiftSlide(1))
-
-//   // Transition events
-//   container.addEventListener('transitionend', checkIndex)
-
-//   function dragStart(e) {
-//     e = e || window.event
-//     e.preventDefault()
-//     posInitial = container.offsetLeft
-//     if (e.type === 'touchstart')
-//       posX1 = e.touches[0].clientX
-//     else {
-//       posX1 = e.clientX
-//       document.onmouseup = dragEnd
-//       document.onmousemove = dragAction
-//     }
-//   }
-
-//   function dragAction(e) {
-//     e = e || window.event
-//     if (e.type === 'touchmove') {
-//       posX2 = posX1 - e.touches[0].clientX
-//       posX1 = e.touches[0].clientX
-//     } else {
-//       posX2 = posX1 - e.clientX
-//       posX1 = e.clientX
-//     }
-//     container.style.left = (container.offsetLeft - posX2) + "px"
-//   }
-
-//   function dragEnd() {
-//     posFinal = container.offsetLeft
-//     if (posFinal - posInitial < -threshold)
-//       shiftSlide(1, 'drag')
-//     else if (posFinal - posInitial > threshold)
-//       shiftSlide(-1, 'drag')
-//     else
-//       container.style.left = (posInitial) + "px"
-//     document.onmouseup = null
-//     document.onmousemove = null
-//   }
-
-//   function shiftSlide(dir, action) {
-//     container.classList.add('shifting')
-//     if (allowShift) {
-//       if (!action)
-//         posInitial = container.offsetLeft
-//       if (dir == 1) {
-//         container.style.left = (posInitial - itemWidth) + "px"
-//         index++
-//       } else if (dir == -1) {
-//         container.style.left = (posInitial + itemWidth) + "px"
-//         index--
-//       }
-//     }
-//     allowShift = false
-//   }
-
-//   function checkIndex() {
-//     container.classList.remove('shifting')
-//     if (index === -1) {
-//       container.style.left = -(itemsNumber * itemWidth) + "px"
-//       index = itemsNumber - 1
-//     }
-//     if (index === itemsNumber) {
-//       container.style.left = -(1 * itemWidth) + "px"
-//       index = 0
-//     }
-//     allowShift = true
-//   }
-// }
-
-// // slide(container, items, prev, next)
-// slide(container, items)
